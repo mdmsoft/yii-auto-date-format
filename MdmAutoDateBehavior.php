3bf68d28ab668994ce6a243154c588028f341177
mdmunir <misbahuldmunir@gmail.com>
2014-05-27 19:19:59 +0700

    Prepare to release

---
+++ MdmAutoDateBehavior.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Description of MdmAutoDateBehavior
+ *
+ * @author Misbahul D Munir, misbahuldmunir@gmail.com
+ * 
+ */
+class MdmAutoDateBehavior extends CBehavior
+{
+    /**
+     *
+     * @var array
+     * Mapping attribute from logical to phisical 
+     */
+    public $attributes = array();
+    
+    /**
+     *
+     * @var string 
+     *  
+     */
+    public $logicalFormat = 'd-m-Y';
+    
+    /**
+     *
+     * @var string 
+     */
+    public $physicalFormat = 'Y-m-d';
+    
+    public function __get($param)
+    {
+        if (isset($this->attributes[$param])) {
+            return $this->convertToLogical($this->owner->{$this->attributes[$param]});
+        } else {
+            return parent::__get($name);
+        }
+    }
+
+    public function __set($param, $value)
+    {
+        if (isset($this->attributes[$param])) {
+            $this->owner->{$this->attributes[$param]} = $this->convertToPhysical($value);
+        } else {
+            parent::__set($name, $value);
+        }
+    }
+
+    private function convertToPhysical($value)
+    {
+        if (empty($value)) {
+            return null;
+        }
+        $date = DateTime::createFromFormat($this->logicalFormat, $value);
+        return $date->format($this->physicalFormat);
+    }
+
+    private function convertToLogical($value)
+    {
+        if (empty($value)) {
+            return null;
+        }
+        $date = DateTime::createFromFormat($this->physicalFormat, $value);
+        return $date->format($this->logicalFormat);
+    }
+
+    public function canGetProperty($name)
+    {
+        return isset($this->attributes[$name]) || parent::canGetProperty($name);
+    }
+
+    public function canSetProperty($name)
+    {
+        return isset($this->attributes[$name]) || parent::canSetProperty($name);
+    }
+}
